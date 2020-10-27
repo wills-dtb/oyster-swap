bc741a334d0a2296b7bdf281f20b086253bde55a
Nathaniel Parke <nathaniel.parke@gmail.com>
2020-10-27 12:00:35 +0800

    Updates

---
+++ precache-manifest.c245760c6955d80b32f75b659b066050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14643ef43b1b48d5d5082256ceb6484",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b983a5597b9c80469fd",
+    "url": "./static/css/2.b5a80ab8.chunk.css"
+  },
+  {
+    "revision": "f7a08e44e7680ae2f347",
+    "url": "./static/css/main.a039c542.chunk.css"
+  },
+  {
+    "revision": "5b983a5597b9c80469fd",
+    "url": "./static/js/2.c78b37d9.chunk.js"
+  },
+  {
+    "revision": "6ff87620e4f720e979dad85ffac87bda",
+    "url": "./static/js/2.c78b37d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a08e44e7680ae2f347",
+    "url": "./static/js/main.cbbd5012.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  }
+]);
