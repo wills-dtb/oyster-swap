abf83f37422a0c1fc7bbd83369c0a39566d158cd
bartosz-lipinski <264380+bartosz-lipinski@users.noreply.github.com>
2020-11-01 12:47:35 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0d0bdfa09b6e81529ee180a6d9e368be.js"
+  "./precache-manifest.d19934a823d2f14152888865a13c6983.js"
 );
 
 self.addEventListener('message', (event) => {
