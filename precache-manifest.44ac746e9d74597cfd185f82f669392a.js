ccda1ef4a5764bb886b7c22d096ef54ec525cd55
bartosz-lipinski <264380+bartosz-lipinski@users.noreply.github.com>
2021-03-04 22:46:53 -0600

    Updates

--- precache-manifest.44ac746e9d74597cfd185f82f669392a.js
+++ precache-manifest.44ac746e9d74597cfd185f82f669392a.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "de43d2ad8f89a5e2d0fee521c355452f",
+    "revision": "731cc8dce91b90ad61e323f0ae14650a",
     "url": "./index.html"
   },
   {
-    "revision": "69829a22ffa891562305",
+    "revision": "fc84612c78dc97cd898b",
     "url": "./static/css/2.b5a80ab8.chunk.css"
   },
   {
-    "revision": "87f4504499aa8b87141a",
+    "revision": "d8a7da184264268dbf10",
     "url": "./static/css/main.6b3e706b.chunk.css"
   },
   {
-    "revision": "69829a22ffa891562305",
-    "url": "./static/js/2.4e852428.chunk.js"
+    "revision": "fc84612c78dc97cd898b",
+    "url": "./static/js/2.326754b9.chunk.js"
   },
   {
     "revision": "f5a8b6868b8c2bc8aba74532e51be46b",
-    "url": "./static/js/2.4e852428.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.326754b9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "87f4504499aa8b87141a",
-    "url": "./static/js/main.0ca059c6.chunk.js"
+    "revision": "d8a7da184264268dbf10",
+    "url": "./static/js/main.5f9fb26f.chunk.js"
   },
   {
     "revision": "d852897cea0a694b3726",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6501e7c46fad38d1b6937700f0003a13.js"
+  "./precache-manifest.44ac746e9d74597cfd185f82f669392a.js"
 );
 
 self.addEventListener('message', (event) => {
